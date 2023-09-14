c9f32fac608fa063c4b1163d4b38a32ef4b2716e
Caroline <wanguic49@gmail.com>
2023-09-14 23:57:22 +0300

    few changes made

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-function tellJoke(){
-    console.log('haha so funny');
+async function getJoke(){
+    const apiUrl = 'https://icanhazdadjoke.com';
+
+try {
+    const response = await fetch(apiUrl, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const joke = data.joke;
+
+    console.log(joke);
+} catch (e) {
+    console.log(e);
 }
+}
+
